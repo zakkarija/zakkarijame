2d9d0d87e9eac6f92bde228a06f7360e19fa466f
Zak Micallef <avatarziko619@gmail.com>
2025-05-13 19:06:12 +0200

    Added vercel package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import Background from "~/components/Background";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Zakkarija Micallef",
@@ -39,6 +40,7 @@ export default function RootLayout({
       <body>
         <Background />
         {children}
+        <Analytics />
       </body>
     </html>
   );
